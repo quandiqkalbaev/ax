efd27a61b2872885ad34345bf58579fd0c2a7791
quandiqdev <quandiqdev@gmail.com>
2023-10-05 03:18:04 +0500

    images added

--- js/main.js
+++ js/main.js
@@ -2,17 +2,25 @@ import { Viewer } from "@photo-sphere-viewer/core";
 
 let modal = document.querySelector(".modal__wrapper");
 let closeBtn = document.querySelector(".modal__close");
-let modalOpen = document.querySelector(".modal__item");
+let modals = document.querySelectorAll(".modal__item");
+let viewer;
 
-modalOpen.addEventListener("click", function () {
-  modal.style.display = "block";
-});
-closeBtn.addEventListener("click", function () {
-  modal.style.display = "none";
-});
+modals.forEach((e, index) => {
+  e.addEventListener("click", function () {
+    modal.style.display = "block";
 
-const viewer = new Viewer({
-  container: "viewer",
-  panorama: "../images/4.jpg",
-  mousewheelCtrlKey: true,
+    if (viewer) {
+      viewer.destroy();
+    }
+
+    viewer = new Viewer({
+      container: "viewer",
+      panorama: `../images/${index + 1}.jpg`,
+      mousewheelCtrlKey: true,
+    });
+  });
+
+  closeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
 });
